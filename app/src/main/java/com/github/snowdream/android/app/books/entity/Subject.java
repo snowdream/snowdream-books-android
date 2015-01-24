dd8f37ae48c2cc40c511439fa510532db5935889
snowdream <yanghui1986527@gmail.com>
2015-01-24 21:37:09 +0800

    update:  data

--- app/src/main/java/com/github/snowdream/android/app/books/entity/Subject.java
+++ app/src/main/java/com/github/snowdream/android/app/books/entity/Subject.java
@@ -2,24 +2,31 @@ package com.github.snowdream.android.app.books.entity;
 
 import android.text.TextUtils;
 
+import java.util.Locale;
+
 /**
  * Created by hui.yang on 2015/1/24.
  */
 public class Subject {
-    private String name = null;
+    private String zh_name = null;
+    private String en_name = null;
     /**
      * for example, ANT000000 as a key for Antiques & Collectibles
      */
     private String key = null;
     private String url = null;
+    private String language = null;
 
    private Subject(){
    };
 
-    public Subject(String key, String name,String url){
+    public Subject(String key, String en_name, String zh_name,String url){
         this.key = key;
-        this.name = name;
+        this.en_name = en_name;
+        this.zh_name = zh_name;
         this.url = url;
+
+        language = Locale.getDefault().getLanguage();
     }
 
     /**
@@ -28,15 +35,31 @@ public class Subject {
      * @return
      */
     public boolean isValid() {
-        return !TextUtils.isEmpty(key) && !TextUtils.isEmpty(name) &&  !TextUtils.isEmpty(url);
+        return !TextUtils.isEmpty(key) && !TextUtils.isEmpty(en_name) &&  !TextUtils.isEmpty(url);
+    }
+
+    public String getZh_name() {
+        return zh_name;
+    }
+
+    public void setZh_name(String zh_name) {
+        this.zh_name = zh_name;
+    }
+
+    public String getEn_name() {
+        return en_name;
+    }
+
+    public void setEn_name(String en_name) {
+        this.en_name = en_name;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        if (language.equalsIgnoreCase("zh")){
+            return  getZh_name();
+        }else{
+            return getEn_name();
+        }
     }
 
     public String getKey() {
